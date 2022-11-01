ec5a6f9a424c84b544e3cec45528c76906d2bad6
Louis Perruchon <louisperruchon@sfs-wifi-dhcp-10-142-4-212.wifi.sfsu.edu>
2022-11-01 14:18:06 -0700

    start task 5, I didn't do the plot associated to my data analytics

--- Louis_folder/new_task.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
-
-import sqlite3
-salary_data = pd.read_csv('/Users/louisperruchon/documents/project_ds311/data/salary_data_states (2).csv', sep=";")
-print(salary_data)
